ffaad1c69324724881dfe54c0ef1b82c86deb521
Gokul teja <147913925+gokulteja49@users.noreply.github.com>
2025-05-05 18:40:24 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-def build_custom_model(input_shape=(32, 32, 3), num_classes=10):  # Change num_classes to 100 for CIFAR-100
+def build_custom_model(input_shape=(32, 32, 3), num_classes=10):  
     model = Sequential([
         Conv2D(32, (3,3), activation='relu', input_shape=input_shape),
         MaxPooling2D((2,2)),
@@ -10,7 +10,7 @@ def build_custom_model(input_shape=(32, 32, 3), num_classes=10):  # Change num_c
         Flatten(),
         Dense(256, activation='relu'),
         Dropout(0.5),
-        Dense(num_classes, activation='softmax')  # Ensure the number of classes matches the dataset
+        Dense(num_classes, activation='softmax')  
     ])
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
